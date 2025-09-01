8e9c0f728a9ff474cf5d185f461ca489d99b108a
Norbey Gutierrez <norbeyd_gutierrez@soy.sena.edu.co>
2025-09-01 18:01:30 -0500

    creando endpoints y apis con fastapi

---
+++ app/routers/customers.py
@@ -0,0 +1,49 @@
+
+from fastapi import APIRouter, Depends, HTTPException, status
+from sqlmodel import select
+from db import SessionDep
+from models import CreateCustomer, UpdateCustomer, Customer
+
+
+router = APIRouter()
+
+
+@router.post("/customers", response_model=Customer, tags=["customers"])
+async def create_customer(customer_data: CreateCustomer, session: SessionDep):
+    customer = Customer.model_validate(customer_data.model_dump())
+    session.add(customer)
+    session.commit()
+    session.refresh(customer)
+    return customer
+
+@router.get("/customers/{customer_id}", response_model=Customer, tags=["customers"])
+async def read_customer(customer_id: int, session: SessionDep):
+    customer_db = session.get(Customer, customer_id)
+    if not customer_db:
+        return HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Customer does not exist")
+    return customer_db
+
+@router.patch("/customers/{customer_id}", response_model=Customer, status_code=status.HTTP_201_CREATED, tags=["customers"])
+async def update_customer(customer_id: int, customer_data: UpdateCustomer, session: SessionDep):
+    customer_db = session.get(Customer, customer_id)
+    if not customer_db:
+        return HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Customer does not exist")
+    customer_data_dict = customer_data.model_dump(exclude_unset=True)
+    customer_db.sqlmodel_update(customer_data_dict)
+    session.commit()
+    session.refresh(customer_db)
+    return customer_db
+
+@router.delete("/customers/{customer_id}", tags=["customers"])
+async def delete_customer(customer_id: int, session: SessionDep):
+    customer_db = session.get(Customer, customer_id)
+    if not customer_db:
+        return HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Customer does not exist")
+    session.delete(customer_db)
+    session.commit()
+    return {"detail": "ok"}
+
+
+@router.get("/customers", response_model=list[Customer], tags=["customers"])
+async def list_customers(session: SessionDep):
+    return session.exec(select(Customer)).all()
---
+++ db.py
@@ -0,0 +1,22 @@
+from typing import Annotated
+from fastapi import Depends
+from fastapi import FastAPI
+from sqlmodel import SQLModel, Session, create_engine
+
+
+sqlite_name = "db.sqlite3"
+sqlite_url = f"sqlite:///{sqlite_name}"
+
+engine = create_engine(sqlite_url)
+
+def create_all_tables(app: FastAPI):
+    SQLModel.metadata.create_all(engine)
+    yield
+
+def get_session():
+    with Session(engine) as session:
+        yield session
+
+
+# Registro de sesion para todos los endpoints
+SessionDep = Annotated[Session, Depends(get_session)]
