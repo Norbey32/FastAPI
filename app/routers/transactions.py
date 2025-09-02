45e06ece8a2b743bd130315e2518bb901005103b
Norbey Gutierrez <norbeyd_gutierrez@soy.sena.edu.co>
2025-09-01 20:20:43 -0500

    creando transacciones

---
+++ app/routers/transactions.py
@@ -0,0 +1,27 @@
+from fastapi import APIRouter, HTTPException, status
+from sqlmodel import select
+from db import SessionDep
+from models import Transaction, TransactionCreate, Customer
+
+
+router = APIRouter()
+
+@router.post("/transactions", tags=["transactions"])
+async def create_transaction(transaction_data: TransactionCreate, session: SessionDep):
+    transaction_data_dict = transaction_data.model_dump()
+    customer = session.get(Customer, transaction_data_dict["customer_id"])
+    if not customer:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Customer not found")
+    
+    transaction_db = Transaction.model.validate(transaction_data_dict)
+    session.add(transaction_db)
+    session.commit()
+    session.refresh(transaction_db)
+    return transaction_db
+    
+
+@router.get("/transactions", tags=["transactions"])
+async def list_transactions(session: SessionDep):
+    query = select(Transaction)
+    transactions = session.exec(query).all()
+    return transactions
