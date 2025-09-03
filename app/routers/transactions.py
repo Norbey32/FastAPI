ba2af47a02f1bab6d3753b1730c0d889971be6c2
Norbey Gutierrez <norbeyd_gutierrez@soy.sena.edu.co>
2025-09-03 15:15:25 -0500

    Creando archivos de pruebas

--- app/routers/transactions.py
+++ app/routers/transactions.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, HTTPException, status
+from fastapi import APIRouter, HTTPException, Query, status
 from sqlmodel import select
 from db import SessionDep
 from models import Transaction, TransactionCreate, Customer
@@ -21,7 +21,11 @@ async def create_transaction(transaction_data: TransactionCreate, session: Sessi
     
 
 @router.get("/transactions", tags=["transactions"])
-async def list_transactions(session: SessionDep):
-    query = select(Transaction)
+async def list_transactions(
+    session: SessionDep,
+    skip: int = Query(0, description="Registros a omitir"),
+    limit: int = Query(2, description="Número máximo de registros")
+):
+    query = select(Transaction).offset(skip).limit(limit)
     transactions = session.exec(query).all()
     return transactions
--- create_multiples_transactions.py
+++ create_multiples_transactions.py
@@ -1,8 +1,8 @@
 from sqlmodel import Session
+
+
 from db import engine
 from models import Customer, Transaction
-
-
 
 session = Session(engine)
 customer = Customer(
@@ -18,9 +18,8 @@ for x in range(100):
     session.add(
         Transaction(
             customer_id=customer.id,
-            amount=100,
             description=f"Test number {x}",
-            amount=10 * x,
+            amount=10 * x
         )
     )
     session.commit()
